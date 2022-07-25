f262941c507412e87f4abcc831c9a010882bfdab
Rafael Felix <82686903+Rafaelrfo88@users.noreply.github.com>
2022-07-25 17:25:15 -0300

    Realizando teste de atualização

--- Desafio01.py
+++ Desafio01.py
@@ -1,4 +1,4 @@
-nome = input('Qual o seu nome? ')
+nome = input('Qual é o seu nome? ')
 print('Ola', '\033[31m', nome,'\033[m! Prazer em tem conhecer!')
 
 #print('E um prazer te conhecer {}!'.format(nome))  ||podemos utilizar dessa maneira tambem
